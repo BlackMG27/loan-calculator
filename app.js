5c998c9e3b1a4abb6056ebb393ffc022830d1028
BlackMG27 <oike279@gmail.com>
2019-07-31 12:04:00 -0400

    added the loader and improved user experience

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 document
     .getElementById('loan-form')
-    .addEventListener('submit', calculateResults);
+    .addEventListener('submit', function (e) {
+        //hide results
+        document
+            .getElementById('results')
+            .style
+            .display = 'none';
+        //show loader
+        document
+            .getElementById('loading')
+            .style
+            .display = 'block';
 
-function calculateResults(e) {
-    console.log('Calculating...');
+        setTimeout(calculateResults, 2500);
 
+        e.preventDefault();
+    });
+
+function calculateResults() {
+
+    //set up the variables from index
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
     const monthlyPayment = document.getElementById('monthly-payment');
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
-
+    //set up the calculations
     const principal = parseFloat(amount.value);
     const calculatedInterest = parseFloat(interest.value) / 100 / 12;
     const calculatedPayments = parseFloat(years.value) * 12;
@@ -22,20 +37,38 @@ function calculateResults(e) {
 
     if (isFinite(monthly)) {
         //display our results in the input fields
-
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        //show results
+        document
+            .getElementById('results')
+            .style
+            .display = 'block';
+        //hide loader
+        document
+            .getElementById('loading')
+            .style
+            .display = 'none';
 
     } else {
         showError('Please check your numbers');
 
     }
 
-    e.preventDefault();
 }
 
 function showError(error) {
+    //hide results
+    document
+        .getElementById('results')
+        .style
+        .display = 'none';
+    //hide loader
+    document
+        .getElementById('loading')
+        .style
+        .display = 'none';
     //create a div
     const errorDiv = document.createElement('div');
     //get elements
